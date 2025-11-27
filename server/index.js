c50c27d16c6dc427869e9bd7794f7cdd239b2c10
Jiancardeus <jia.rivera@duocuc.cl>
2025-11-27 15:26:39 -0300

    Permitir conexiones HTTP
    se modifico el manifest y se agrego network_security... xml para gestionar las conexiones inseguras

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 // --- Importar Rutas ---
-// Nota: Eliminamos la importación del modelo 'User' aquí, ya que se usa dentro de las rutas.
 const authRoutes = require('./routes/auth');
 const cardRoutes = require('./routes/cards');
 
@@ -16,12 +15,11 @@ app.use(express.json());
 
 // --- Conexión a MongoDB Atlas ---
 // Esta es tu URL de conexión real, la dejamos aquí.
-const MONGODB_URI = 'mongodb+srv://<app_admin>:<app123>@app.w280lxg.mongodb.net/?appName=app';
+const MONGODB_URI = 'mongodb+srv://app_admin:app123@app.w280lxg.mongodb.net/?appName=app';
 
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('✅ Conectado a MongoDB Atlas'))
   .catch(err => console.error('❌ Error de conexión a MongoDB:', err));
-
 
 // --- Integración de Rutas Modulares ---
 
@@ -31,13 +29,10 @@ app.use('/api/auth', authRoutes);
 // 2. Rutas de Cartas (Biblioteca)
 app.use('/api/cards', cardRoutes);
 
-
 // --- Endpoint de Prueba ---
-// Endpoint simple para verificar que el servidor está activo
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'API de Realms in Discord TCG está activa.' });
 });
-
 
 // --- Inicio del Servidor ---
 app.listen(PORT, () => {
